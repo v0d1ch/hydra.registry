2663099f7fcb4dd3bfb38178e65645481d1cc1d8
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2024-10-11 16:47:24 +0200

    Add to/from json and use the ApiMsg

--- common/src/Common/Api.hs
+++ common/src/Common/Api.hs
@@ -1,21 +1,27 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 
 module Common.Api where
 
+import Data.Aeson
 import Data.Text (Text)
+import GHC.Generics
 
 commonStuff :: String
-commonStuff = "Hydra Head Registry is public registry of Hydra heads. In order to get your Head listed please submit a PR to the github repository (https://github.com/v0d1ch/hydra.registry) and add <IP:PORT> to the list of Heads at https://github.com/v0d1ch/hydra.registry/blob/main/config/common/heads or send an email to sasa [dot] bogicevic [at] pm [dot] me"
+commonStuff = "Hydra Head Registry is public registry of Hydra heads. In order to get your Head listed send email to sasa [dot] bogicevic [at] pm [dot] me and specify <IP:PORT> for your Hydra Head"
 
 data HeadStatus
   = Idle
   | Initializing
   | Open
   | Closed
-  deriving stock (Show, Eq)
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (ToJSON, FromJSON)
 
 data ApiMsg = ApiMsg
   { _apiMsg_headId :: Text -- TODO: use the real type from Hydra here eventually
   , _apiMsg_status :: HeadStatus
   }
-  deriving stock (Show, Eq)
+  deriving stock (Show, Eq, Generic)
+  deriving anyclass (ToJSON, FromJSON)
