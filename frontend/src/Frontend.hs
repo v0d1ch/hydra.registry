afc62ffa0e1979d4e9cf8714fa96ab2ea5f2d8a6
v0d1ch <sasha.bogicevic@iohk.io>
2024-10-13 14:52:40 +0200

    Use tailwind css

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -40,10 +40,20 @@ frontend =
     { _frontend_head = do
         el "title" $ text "Hydra Head Registry"
         elAttr "meta" ("name" =: "viewport" <> "content" =: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no") blank
+
+        elAttr "link" ("rel" =: "preconnect" <> "href" =: "https://fonts.googleapis.com") blank
+        elAttr "link" ("rel" =: "preconnect" <> "href" =: "https://fonts.gstatic.com") blank
+        elAttr "link" ("href" =: "https://fonts.googleapis.com/css2?family=Inria+Sans:wght@300&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Krona+One&family=Rajdhani:wght@300;400;500;600;700&display=swap" <> "rel" =: "stylesheet") blank
+        elAttr "link" ("rel" =: "stylesheet" <> "href" =: "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0") blank
+        elAttr "script" ("src" =: "https://cdn.tailwindcss.com") blank
+
+        -- Highlight JS for syntax highlighting
+        elAttr "link" ("rel" =: "stylesheet" <> "href" =: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/default.min.css") blank
+        elAttr "script" ("src" =: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js") blank
         elAttr "script" ("type" =: "application/javascript" <> "src" =: $(static "lib.js")) blank
         elAttr "link" ("href" =: $(static "main.css") <> "type" =: "text/css" <> "rel" =: "stylesheet") blank
     , _frontend_body = do
-        el "h1" $ text "Hydra Head Registry"
+        elClass "h1" "mdcTypography" $ text "Hydra Head Registry"
         el "p" $ text $ T.pack commonStuff
         prerender_ (text "Loading Heads...") $ do
           pb <- getPostBuild
@@ -97,8 +107,13 @@ displayHead ws = do
       (_webSocket_recv ws)
   _ <-
     elClass "div" "container" $ do
-      el "p" $ dynText headId
-      el "p" $ dynText headStatus
+      elClass "table" "table-auto" $ do
+        el "tr" $ do
+          el "th" $ text "Head ID:"
+          el "th" $ text "Head Status:"
+        el "tr" $ do
+          el "td" $ dynText headId
+          el "td" $ dynText headStatus
   return ()
 
 parseHeadId :: ByteString -> Maybe T.Text
